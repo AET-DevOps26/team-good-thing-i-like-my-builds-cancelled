0912aa4c6813dbb0e6fa14b10a0ddd49303993fe
Anian Schleyer <anian.schleyer@tum.de>
2026-06-04 01:41:37 +0200

    Generate server code

---
+++ apps/server/logbook-service/src/main/java/dev/gtilmbc/logbookservice/generated/model/Station.java
@@ -0,0 +1,107 @@
+package dev.gtilmbc.logbookservice.generated.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import org.springframework.lang.Nullable;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import jakarta.annotation.Generated;
+
+/**
+ * Station
+ */
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2026-06-04T01:38:53.020754+02:00[Europe/Berlin]", comments = "Generator version: 7.22.0")
+public class Station {
+
+  private @Nullable String id;
+
+  private @Nullable String title;
+
+  public Station id(@Nullable String id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Get id
+   * @return id
+   */
+  
+  @Schema(name = "id", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("id")
+  public @Nullable String getId() {
+    return id;
+  }
+
+  @JsonProperty("id")
+  public void setId(@Nullable String id) {
+    this.id = id;
+  }
+
+  public Station title(@Nullable String title) {
+    this.title = title;
+    return this;
+  }
+
+  /**
+   * Get title
+   * @return title
+   */
+  
+  @Schema(name = "title", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("title")
+  public @Nullable String getTitle() {
+    return title;
+  }
+
+  @JsonProperty("title")
+  public void setTitle(@Nullable String title) {
+    this.title = title;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Station station = (Station) o;
+    return Objects.equals(this.id, station.id) &&
+        Objects.equals(this.title, station.title);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, title);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Station {\n");
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    title: ").append(toIndentedString(title)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(@Nullable Object o) {
+    return o == null ? "null" : o.toString().replace("\n", "\n    ");
+  }
+}
+
