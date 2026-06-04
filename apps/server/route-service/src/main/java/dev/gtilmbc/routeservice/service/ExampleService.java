90b80e5ad2a708feb3d9d8c52b86ea70ac77c765
Anian Schleyer <anian.schleyer@tum.de>
2026-06-04 02:03:18 +0200

    Use example for route service

--- apps/server/route-service/src/main/java/dev/gtilmbc/routeservice/service/ExampleService.java
+++ apps/server/route-service/src/main/java/dev/gtilmbc/routeservice/service/ExampleService.java
@@ -1,49 +1,20 @@
 package dev.gtilmbc.routeservice.service;
 
-import dev.gtilmbc.routeservice.dto.ExampleCreateRequest;
-import dev.gtilmbc.routeservice.dto.ExampleDto;
-import dev.gtilmbc.routeservice.model.Example;
+import dev.gtilmbc.routeservice.generated.model.Station;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ExampleService {
-    ArrayList<Example> examples = new ArrayList<>();
 
-    public List<ExampleDto> getAll() {
-        return examples.stream().map(this::toDto).toList();
+    public List<Station> getAll() {
+        var station = new Station();
+        station.setId("TestId");
+        station.setTitle("Test Station");
+        return List.of(station);
     }
 
-    public ExampleDto get(Long id) {
-        return examples.stream()
-            .filter(e -> e.getId().equals(id))
-            .findFirst()
-            .map(this::toDto)
-            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Example not found"));
-    }
-
-    public ExampleDto save(ExampleCreateRequest request) {
-        Example example = toEntity(request);
-
-        // set id as random long
-        example.setId(Math.round(Math.random() * 1000000));
-        examples.add(toEntity(request));
-        return get(example.getId());
-    }
-
-    private ExampleDto toDto(Example example) {
-        return new ExampleDto(example.getId(), example.getName());
-    }
-
-    private Example toEntity(ExampleCreateRequest request) {
-        Example example = new Example();
-        example.setName(request.name());
-        return example;
-    }
 }
