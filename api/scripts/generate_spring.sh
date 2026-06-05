b8b783cbabc3f6ac48571630d9c05af63bdc4a45
Anian Schleyer <98647423+anian03@users.noreply.github.com>
2026-06-05 15:57:55 +0200

    `Feature`: Generate Server from openapi spec (#24)
    * Add templates for k8s
    
    * Add helm setup
    
    * Add image names
    
    * Add client to deployment
    
    * Remove namespace from ingress
    
    * Add database to deployment
    
    * Setup database deployment
    
    * Add pgadmin to ingress
    
    * Correct genai port
    
    * Increase resources
    
    * Fix database not persisting
    
    * Add generator script
    
    * Remove old OpenAPI
    
    * Improve generation
    
    * Generate server code
    
    * Add openapitools
    
    * Update server URL
    
    * Add API path
    
    * Use example for route service
    
    * Fix api path

---
+++ api/scripts/generate_spring.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Generate Spring Stubs for API
+
+set -euo pipefail
+
+for path in route logbook; do
+
+    echo "Generating code for $path..."
+
+    openapi-generator-cli generate \
+        -g spring \
+	--global-property apis,models,supportingFiles=ApiUtil.java \
+        -i api/openapi.yaml \
+        -o apps/server/${path}-service \
+    	--additional-properties=basePackage=dev.gtilmbc.${path}service.generated,apiPackage=dev.gtilmbc.${path}service.generated.api,modelPackage=dev.gtilmbc.${path}service.generated.model,useTags=true,interfaceOnly=true
+
+done
--- libs/contracts/openapi/src/lib/generated/api/api.ts
+++
@@ -1,4 +0,0 @@
-export * from './examples.service';
-import { ExamplesApi } from './examples.service';
-export * from './examples.serviceInterface';
-export const APIS = [ExamplesApi];
