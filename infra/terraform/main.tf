e4b70bdaeb01c4bf627ea1a51795195da3186a1f
Jan-Thurner <107639007+Jan-Thurner@users.noreply.github.com>
2026-06-08 22:50:53 +0200

    Change VM size

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -52,7 +52,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                            = local.vm
   resource_group_name             = azurerm_resource_group.rg.name
   location                        = azurerm_resource_group.rg.location
-  size                            = "Standard_B1s"
+  size                            = "Standard_D2s_v3"
   admin_username                  = var.admin_username
   network_interface_ids           = [azurerm_network_interface.nic.id]
   disable_password_authentication = true
