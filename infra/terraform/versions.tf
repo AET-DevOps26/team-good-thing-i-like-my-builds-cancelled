c975f0163cc6c4c509c428caed4f4408010031a3
Jan-Thurner <107639007+Jan-Thurner@users.noreply.github.com>
2026-06-08 21:24:01 +0200

    formatting

--- infra/terraform/versions.tf
+++ infra/terraform/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = ">= 1.6.0"
 
   backend "azurerm" {
-      resource_group_name = "GTILMBC"
-      storage_account_name = "gtilmbctfstate"
-      container_name       = "tfstate"
-      key                  = "terraform.tfstate"
-      use_azuread_auth = true
+    resource_group_name  = "GTILMBC"
+    storage_account_name = "gtilmbctfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_azuread_auth     = true
   }
 
   required_providers {
