678fd2fcd89a94a9d2b4072092de6578d564ab56
Jan-Thurner <107639007+Jan-Thurner@users.noreply.github.com>
2026-06-08 22:47:21 +0200

    change IP generation from dynamic to static

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -32,8 +32,8 @@ resource "azurerm_public_ip" "pip" {
   name                = local.pip
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
-  sku                 = "Basic"
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_network_interface" "nic" {
