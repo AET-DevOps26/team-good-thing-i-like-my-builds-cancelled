3c8115bf191fc5916503d8cf0992924350a0dd05
Jan-Thurner <107639007+Jan-Thurner@users.noreply.github.com>
2026-06-14 15:53:39 +0200

    open port 22

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -36,6 +36,30 @@ resource "azurerm_public_ip" "pip" {
   sku                 = "Standard"
 }
 
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${local.nic}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = local.tags
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
 resource "azurerm_network_interface" "nic" {
   name                = local.nic
   location            = azurerm_resource_group.rg.location
