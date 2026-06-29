22d3cd96abf1c3a316fd067bbb0732410f7f2939
Anian Schleyer <anian.schleyer@tum.de>
2026-06-29 14:00:44 +0200

    Update k8s deployment

--- apps/server/route-service/src/main/java/dev/gtilmbc/routeservice/service/TimetableService.java
+++ apps/server/route-service/src/main/java/dev/gtilmbc/routeservice/service/TimetableService.java
@@ -17,6 +17,8 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Locale;
