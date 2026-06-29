dd617775c662584a649dd8bee35e7a3dcfcfc3c9
Anian Schleyer <98647423+anian03@users.noreply.github.com>
2026-06-29 14:07:10 +0200

    Fix accidental commit

--- apps/server/route-service/src/main/java/dev/gtilmbc/routeservice/service/TimetableService.java
+++ apps/server/route-service/src/main/java/dev/gtilmbc/routeservice/service/TimetableService.java
@@ -17,8 +17,6 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Locale;
