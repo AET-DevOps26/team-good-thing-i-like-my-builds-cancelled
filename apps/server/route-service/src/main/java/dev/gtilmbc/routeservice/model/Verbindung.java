305fe8895c22c1383eb1f0e257bb58660cb9a135
Anian Schleyer <98647423+anian03@users.noreply.github.com>
2026-07-14 22:49:06 +0200

    `Bugfix`: Fix some routes not being found (#66)
    * Remove walking connections
    
    * Ensure connections is not null

--- apps/server/route-service/src/main/java/dev/gtilmbc/routeservice/model/Verbindung.java
+++ apps/server/route-service/src/main/java/dev/gtilmbc/routeservice/model/Verbindung.java
@@ -4,12 +4,13 @@ import dev.gtilmbc.routeservice.generated.model.TrainConnection;
 import dev.gtilmbc.routeservice.generated.model.TrainSegment;
 
 import java.util.List;
+import java.util.Objects;
 
 public record Verbindung(List<VerbindungAbschnitt> verbindungsAbschnitte) {
     public TrainConnection asConnection() {
         TrainConnection connection = new TrainConnection();
 
-        List<TrainSegment> segments = verbindungsAbschnitte.stream().map(VerbindungAbschnitt::asSegment).toList();
+        List<TrainSegment> segments = verbindungsAbschnitte.stream().map(VerbindungAbschnitt::asSegment).filter(Objects::nonNull).toList();
         connection.setSegments(segments);
 
         connection.setDepartureTime(segments.getFirst().getDepartureTime());
